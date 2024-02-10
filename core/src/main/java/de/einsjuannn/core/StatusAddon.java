f9bdadf19c3809676bcb81f6de6ebf629c93fd08
RappyTV <rappytv@rappytv.com>
2024-02-10 17:05:15 +0100

    Reformat main

--- core/src/main/java/de/einsjuannn/core/StatusAddon.java
+++ core/src/main/java/de/einsjuannn/core/StatusAddon.java
@@ -7,15 +7,14 @@ import net.labymod.api.models.addon.annotation.AddonMain;
 @AddonMain
 public class StatusAddon extends LabyAddon<StatusConfiguration> {
 
-  @Override
-  protected void enable() {
-    this.registerSettingCategory();
-    this.registerListener(new ChatMessageSendListener());
-    this.logger().info("Enabled the Addon");
-  }
+    @Override
+    protected void enable() {
+        this.registerSettingCategory();
+        this.registerListener(new ChatMessageSendListener());
+    }
 
-  @Override
-  protected Class<StatusConfiguration> configurationClass() {
-    return StatusConfiguration.class;
-  }
+    @Override
+    protected Class<StatusConfiguration> configurationClass() {
+        return StatusConfiguration.class;
+    }
 }
